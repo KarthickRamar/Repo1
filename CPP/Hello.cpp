df8bb63a324ff3e417635bf9a085ddaa65aa7982
KarthickRamar <karthie02@gmail.com>
2022-08-09 20:34:03 +0530

    CPP commit

---
+++ CPP/Hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h>
+
+//The main function
+int main()
+{
+	//Prin the text Hello World
+	cout << "Hello World";
+	return 0;  //return type of function is int, so return a int value.
+}
